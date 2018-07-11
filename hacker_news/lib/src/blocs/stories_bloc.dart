83f3e15b9ee019ac664ee276caa7d4a3aaead6d3
Sudhanshu Siddh <ssiddh@iu.edu>
2018-07-11 17:06:00 -0400

    Repository refactor to use Stream builder and future builder

--- hacker_news/lib/src/blocs/stories_bloc.dart
+++ hacker_news/lib/src/blocs/stories_bloc.dart
@@ -7,16 +7,17 @@ import 'package:rxdart/rxdart.dart';
 class StoriesBloc {
   final _repository = Repository();
   final _topIds = PublishSubject<List<int>>();
-  final _items = BehaviorSubject();
-
-  Observable<Map<int, Future<ItemModel>>> items;
+  final _itemsOutput = BehaviorSubject<Map<int, Future<ItemModel>>>();
+  final _itemsFetcher = PublishSubject<int>();
 
   Observable<List<int>> get topIds => _topIds.stream;
 
-  Function(int) get fetchItem => _items.sink.add;
+  Observable<Map<int, Future<ItemModel>>> get items => _itemsOutput.stream;
+
+  Function(int) get fetchItem => _itemsFetcher.sink.add;
 
   StoriesBloc() {
-    items = _items.stream.transform(_itemsTransformer());
+    _itemsFetcher.stream.transform(_itemsTransformer()).pipe(_itemsOutput);
   }
 
   fetchTopIds() async {
@@ -26,7 +27,8 @@ class StoriesBloc {
 
   void dispose() {
     _topIds.close();
-    _items.close();
+    _itemsOutput.close();
+    _itemsFetcher.close();
   }
 
   _itemsTransformer() {
