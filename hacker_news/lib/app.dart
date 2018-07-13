68266c6a6f1bf8e33c361e31314f0697122f1eac
Sudhanshu Siddh <ssiddh@iu.edu>
2018-07-13 11:21:26 -0400

    Logic for recursive fetching of comments

--- hacker_news/lib/app.dart
+++ hacker_news/lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hacker_news/src/blocs/comments_provider.dart';
 import 'package:hacker_news/src/blocs/stories_provider.dart';
 import 'package:hacker_news/src/screens/news_details.dart';
 import 'package:hacker_news/src/screens/news_list.dart';
@@ -6,13 +7,15 @@ import 'package:hacker_news/src/screens/news_list.dart';
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StoriesProvider(
-      child: MaterialApp(
-        title: 'HackerNews',
-        theme: ThemeData(
-          primarySwatch: Colors.orange,
+    return CommentsProvider(
+      child: StoriesProvider(
+        child: MaterialApp(
+          title: 'HackerNews',
+          theme: ThemeData(
+            primarySwatch: Colors.orange,
+          ),
+          onGenerateRoute: routes,
         ),
-        onGenerateRoute: routes,
       ),
     );
   }
@@ -26,7 +29,10 @@ class App extends StatelessWidget {
       );
     } else {
       return MaterialPageRoute(builder: (context) {
-        return NewsDetails();
+        final commentsBloc = CommentsProvider.of(context);
+        final itemId = int.parse(settings.name.replaceFirst("/", ''));
+        commentsBloc.fetchItemWithComments(itemId);
+        return NewsDetails(itemId: itemId,);
       });
     }
   }
--- hacker_news/lib/src/screens/news_details.dart
+++ hacker_news/lib/src/screens/news_details.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 
 class NewsDetails extends StatelessWidget {
+  final itemId;
+  NewsDetails({this.itemId});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Details'),
       ),
-      body: Text('Details will come here'),
+      body: Text('Details will come here $itemId'),
     );
   }
 }
