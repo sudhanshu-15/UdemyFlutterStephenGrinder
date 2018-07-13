1bbbb1f6a4d8c85a5d240ad6e84250ffbf8c7b10
Sudhanshu Siddh <ssiddh@iu.edu>
2018-07-13 15:58:09 -0400

    Added loading indicator block to display while comments are loading

--- hacker_news/lib/src/widgets/comment.dart
+++ hacker_news/lib/src/widgets/comment.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:hacker_news/src/models/item_model.dart';
+import 'package:hacker_news/src/widgets/loading_container.dart';
 
 class Comment extends StatelessWidget {
   final int itemId;
@@ -16,7 +17,7 @@ class Comment extends StatelessWidget {
       future: itemMap[itemId],
       builder: (context, AsyncSnapshot<ItemModel> snapshot) {
         if (!snapshot.hasData) {
-          return Text('Still loading comment..');
+          return LoadingContainer();
         }
 
         final children = <Widget>[
@@ -47,10 +48,23 @@ class Comment extends StatelessWidget {
       child: Card(
         color: Colors.white30,
         child: ListTile(
-          title: Text(item.text),
+          title: buildText(item),
           subtitle: item.by == "" ? Text('Deleted') : Text(item.by),
         ),
       ),
     );
+  }
+
+  Widget buildText(ItemModel item) {
+    final text = item.text
+        .replaceAll('&#x27;', "'")
+        .replaceAll('<p>', "\n\n")
+        .replaceAll("</p>", "")
+        .replaceAll("&#x2F;", "/")
+        .replaceAll("&amp;", "&")
+        .replaceAll("&gt;", "<")
+        .replaceAll("&quot;", '"');
+
+    return Text(text);
   }
 }
